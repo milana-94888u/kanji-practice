fd0701eee074fd906a71876aac64979fc4ee8bfb
Milana <94888u@gmail.com>
2023-07-08 20:33:05 +0200

    [fix] remove redundant tab container scene

--- godot_project/src/lists_select_menu/lists_select_menu.gd
+++ godot_project/src/lists_select_menu/lists_select_menu.gd
@@ -1,15 +1,14 @@
 extends Control
 
 
-@onready var packed_tab_container := preload("res://src/lists_select_menu/tab_container.tscn")
-
-
 func _ready() -> void:
 	remove_child($TabContainer)
-	var tab_container := packed_tab_container.instantiate()
+	var tab_container := TabContainer.new()
+	tab_container.set_anchors_preset(Control.PRESET_FULL_RECT)
 	add_child(tab_container)
 	for kanji_lists_block in KanjiListsManager.kanji_lists:
-		var kanji_lists_block_tab := packed_tab_container.instantiate()
+		var kanji_lists_block_tab := TabContainer.new()
+		kanji_lists_block_tab.set_anchors_preset(Control.PRESET_FULL_RECT)
 		kanji_lists_block_tab.name = kanji_lists_block
 		tab_container.add_child(kanji_lists_block_tab)
 		for kanji_list_name in KanjiListsManager.kanji_lists.get(kanji_lists_block):
